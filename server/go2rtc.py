1c2521b336a06b8ca21380a1de032a9b292999f8
csev1755 <49533775+csev1755@users.noreply.github.com>
2026-06-05 07:25:29 -0500

    cleanup go2rtc device list

--- server/go2rtc.py
+++ server/go2rtc.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 import requests
 import yaml
+from urllib import parse
 
 class Go2RTC:
     def __init__(self, bundle_dir, config, log_level, logger):
@@ -28,19 +29,50 @@ class Go2RTC:
             'log': {'format': 'text', 'level': log_level}
         }
 
+    def get_devices(self, url):
+        response = requests.get(url)
+        data = response.json()
+
+        devices = []
+        video_count = 0
+        for source in data.get('sources', []):
+            url_str = source.get('url', '')
+            if not url_str:
+                continue
+
+            parsed_url = parse.urlparse(url_str)
+            query_params = parse.parse_qs(parsed_url.query)
+            if 'video' not in query_params:
+                continue
+
+            device_name = query_params['video'][0]
+            query_params['video'] = [str(video_count)]
+            new_query = parse.urlencode(query_params, doseq=True)
+            fragment_parts = parsed_url.fragment.split('#')
+            clean_fragment = '#'.join(part for part in fragment_parts if 'hardware' not in part)
+            modified_url = parse.urlunparse(parsed_url._replace(query=new_query, fragment=clean_fragment))
+
+            devices.append({
+                'index': video_count,
+                'name': device_name,
+                'url': modified_url,
+                'source_url': url_str,
+            })
+            video_count += 1
+
+        return devices
+
     def start(self):
         with open(self.config, 'w') as f:
             yaml.dump(self.go2rtc_config, f, default_flow_style=False, sort_keys=False)
 
         self.proc = subprocess.Popen([self.bin, "-c", self.config])
 
-        # Print device list
         for _ in range(10):
             try:
-                response = requests.get('http://127.0.0.1:1984/api/ffmpeg/devices')
-                data = response.json()
-                for source in data.get('sources', []):
-                    self.logger.info(f"Found go2rtc ffmpeg device: {source.get('url')}")
+                devices = self.get_devices("http://127.0.0.1:1984/api/ffmpeg/devices")
+                for device in devices:
+                    self.logger.info(f"Video Device {device['index']} - {device['name']} - {device['url']}")
                 return
             except Exception:
                 time.sleep(0.2)
